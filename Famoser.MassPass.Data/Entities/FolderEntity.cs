4a36d370d37a92756652ffe4fdcf1fd27c1180e3
Florian Moser <git@famoser.ch>
2016-05-19 20:09:08 +0200

    MassPass

---
+++ Famoser.MassPass.Data/Entities/FolderEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Famoser.MassPass.Data.Entities
+{
+    public class FolderEntity
+    {
+        public Guid Id { get; set; }
+        public Guid ParentId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Famoser.MassPass.Data/Entities/NoteEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Famoser.MassPass.Data.Entities
+{
+    public class NoteEntity
+    {
+        public Guid Id { get; set; }
+        public Guid ParentId { get; set; }
+        public Guid TypeId { get; set; }
+        public string Name { get; set; }
+        public string ContentJson { get; set; }
+    }
+}
