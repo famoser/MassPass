3d3252936a94b3d06546db84fc0f30b5c00a760a
Florian Moser <git@famoser.ch>
2016-05-28 16:15:46 +0200

    added new property

--- Famoser.MassPass.Data/Entities/Communications/Response/Entitites/AuthorizedDeviceEntity.cs
+++ Famoser.MassPass.Data/Entities/Communications/Response/Entitites/AuthorizedDeviceEntity.cs
@@ -6,6 +6,7 @@ namespace Famoser.MassPass.Data.Entities.Communications.Response.Entitites
     {
         public Guid DeviceId { get; set; }
         public string DeviceName { get; set; }
+        public DateTime LastRequestDateTime { get; set; }
         public DateTime LastModificationDateTime { get; set; }
         public DateTime AuthorizationDateTime { get; set; }
     }
