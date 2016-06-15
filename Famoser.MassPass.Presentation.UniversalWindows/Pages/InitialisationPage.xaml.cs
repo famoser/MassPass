e6978bbd4b8f5dbfbaba0ad1f95b5225e6e777e5
Florian Moser <git@famoser.ch>
2016-06-15 20:01:11 +0200

    new library, new live

--- Famoser.MassPass.Presentation.UniversalWindows/Pages/InitialisationPage.xaml.cs
+++ Famoser.MassPass.Presentation.UniversalWindows/Pages/InitialisationPage.xaml.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage.Streams;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -12,7 +17,10 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using com.google.zxing;
+using com.google.zxing.multi;
 using ZXing;
 using ZXing.Mobile;
 
@@ -30,24 +38,74 @@ namespace Famoser.MassPass.Presentation.UniversalWindows.Pages
             this.InitializeComponent();
         }
 
-        private MobileBarcodeScanner _scanner;
-        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        private MediaCapture _mediaCapture;
+        private async Task InitializeQrCode()
         {
-            _scanner = new MobileBarcodeScanner(this.Dispatcher);
-            _scanner.UseCustomOverlay = false;
-            _scanner.TopText = "Hold camera up to QR code";
-            _scanner.BottomText = "Camera will automatically scan QR code\r\n\rPress the 'Back' button to cancel";
+            // Find all available webcams
+            DeviceInformationCollection webcamList = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
 
-            var result = await _scanner.Scan();
-            ProcessScanResult(result);
+            // Get the proper webcam (default one)
+            DeviceInformation backWebcam = (from webcam in webcamList
+                                            where webcam.IsEnabled
+                                            select webcam).FirstOrDefault();
+
+            // Initializing MediaCapture
+            _mediaCapture = new MediaCapture();
+            await _mediaCapture.InitializeAsync(new MediaCaptureInitializationSettings
+            {
+                VideoDeviceId = backWebcam.Id,
+                AudioDeviceId = "",
+                StreamingCaptureMode = StreamingCaptureMode.Video,
+                PhotoCaptureSource = PhotoCaptureSource.VideoPreview
+            });
+
+            // Set the source of CaptureElement to MediaCapture
+            BarcodeCaptureElement.Source = _mediaCapture;
+            await _mediaCapture.StartPreviewAsync();
         }
 
-        private async void ProcessScanResult(Result result)
-        {
-            var message = (result != null && !string.IsNullOrEmpty(result.Text)) ? "Found QR code: " + result.Text : "Scanning cancelled";
 
-            MessageDialog diag = new MessageDialog(message);
-            await diag.ShowAsync();
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var devices = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(Windows.Devices.Enumeration.DeviceClass.VideoCapture);
+
+                if (devices.Count > 1)
+                {
+                    await InitializeQrCode();
+
+                    var imgProp = new ImageEncodingProperties { Subtype = "BMP", Width = 600, Height = 800 };
+                    var bcReader = new BarcodeReader();
+
+                    while (true)
+                    {
+                        var stream = new InMemoryRandomAccessStream();
+                        await _mediaCapture.CapturePhotoToStreamAsync(imgProp, stream);
+
+                        stream.Seek(0);
+                        var wbm = new WriteableBitmap(600, 800);
+                        await wbm.SetSourceAsync(stream);
+
+                        var result = bcReader.Decode(wbm);
+
+                        if (result != null)
+                        {
+                            var msgbox = new MessageDialog(result.Text);
+                            await msgbox.ShowAsync();
+                        }
+                    }
+                }
+                else
+                {
+                    var dialog = new MessageDialog("No camera could be found; you have to type in the url manually, sorry :(");
+                    await dialog.ShowAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                // ignored
+            }
         }
     }
 }
